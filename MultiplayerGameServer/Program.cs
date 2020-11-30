a5819e9f00315a03c4da374b9e3a5285d68a7d20
Nissan Cohen <nissanc6@gmail.com>
2020-11-30 16:56:09 +0200

    Adding starting packages

---
+++ MultiplayerGameServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiplayerGameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
