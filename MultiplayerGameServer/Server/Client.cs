a3b0a6e1163d435859f531974c6c35013651abc7
rau eyal <raueyal@gmail.com>
2020-11-30 18:45:22 +0200

    init client server tcp

---
+++ MultiplayerGameServer/Server/Client.cs
@@ -0,0 +1,20 @@
+﻿using GameNetworkingShared.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiplayerGameServer.Server
+{
+    public class Client
+    {
+        public int Id { get; private set; }
+        public TCP Tcp { get; private set; }
+        
+        public Client(int id)
+        {
+            Id = id;
+            Tcp = new TCP(Id);
+        }
+    }
+
+}
