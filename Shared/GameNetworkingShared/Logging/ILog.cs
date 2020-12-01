076b2017de719403919bd5228fbe79ab9ce03a8e
Nissan Cohen <nissanc6@gmail.com>
2020-12-01 17:10:31 +0200

    Adding generic logger thing

---
+++ Shared/GameNetworkingShared/Logging/ILog.cs
@@ -0,0 +1,11 @@
+﻿namespace GameNetworkingShared.Logging
+{
+    public interface ILog
+    {
+        void Debug(string message);
+
+        void Info(string message);
+
+        void Error(string message);
+    }
+}
---
+++ Shared/GameNetworkingShared/Logging/LogFactory.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameNetworkingShared.Logging
+{
+    public class LogFactory : ILog
+    {
+        private static LogFactory instance;
+
+        private List<ILog> Loggers { get; set; }
+
+        public static LogFactory Instance => instance ?? (instance = new LogFactory());
+
+        private LogFactory()
+        {
+            // empty ctor
+        }
+
+        public void SubscribeLogger(ILog logger)
+        {
+            if (Loggers == null)
+            {
+                Loggers = new List<ILog>();
+            }
+
+            if (!Loggers.Contains(logger))
+            {
+                Loggers.Add(logger);
+            }
+        }
+
+        public void UnsubscribeLogger(ILog logger)
+        {
+            if (Loggers == null || !Loggers.Contains(logger))
+            {
+                return;
+            }
+
+            Loggers.Remove(logger);
+        }
+
+        public void Debug(string message)
+        {
+            string sentMessage = AddTimeAndTypeToMessage(message, LogType.Debug);
+            foreach (ILog logger in Loggers)
+            {
+                logger.Debug(sentMessage);
+            }
+        }
+
+        public void Info(string message)
+        {
+            string sentMessage = AddTimeAndTypeToMessage(message, LogType.Info);
+            foreach (ILog logger in Loggers)
+            {
+                logger.Info(sentMessage);
+            }
+        }
+
+        public void Error(string message)
+        {
+            string sentMessage = AddTimeAndTypeToMessage(message, LogType.Error);
+            foreach (ILog logger in Loggers)
+            {
+                logger.Error(sentMessage);
+            }
+        }
+
+        private string AddTimeAndTypeToMessage(string message, LogType logType)
+        {
+            return $"{DateTime.Now:s} - {logType.ToString().ToUpper()} - {message}";
+        }
+    }
+}
