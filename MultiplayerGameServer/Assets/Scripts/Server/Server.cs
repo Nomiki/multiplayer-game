8a3e38ec5fbc2a710db14f9c65203cae0f547133
Nissan Cohen <nissanc6@gmail.com>
2021-01-03 00:13:29 +0200

    take3 success

---
+++ MultiplayerGameServer/Assets/Scripts/Server/Server.cs
@@ -0,0 +1,68 @@
+﻿using GameNetworkingShared.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace MultiplayerGameServer.Server
+{
+    public class Server
+    {
+        public static int MaxPlayers { get; private set; }
+        public static int Port { get; private set; }
+
+        private static TcpListener tcpListener;
+
+        public static ServerUdpHandler UdpHandler { get; set; }
+
+        public static Dictionary<int, Client> Clients { get; private set; }
+
+        public static void Start(int maxPlayers, int port)
+        {
+            MaxPlayers = maxPlayers;
+            Port = port;
+
+            InitializeServerData();
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            UdpHandler = new ServerUdpHandler(Port);
+        }
+
+        private static void TCPConnectCallback(IAsyncResult result)
+        {
+            TcpClient client = tcpListener.EndAcceptTcpClient(result);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+            LogFactory.Instance.Debug($"incoming connection from {client.Client.RemoteEndPoint}...");
+
+            for (int i = 1; i <= MaxPlayers; i++)
+            {
+                if (Clients[i].Tcp.Socket == null)
+                {
+                    Clients[i].Tcp.Connect(client);
+                    return;
+                }
+            }
+
+            LogFactory.Instance.Debug($"Could not connect {client.Client.RemoteEndPoint}. Reached max clients {MaxPlayers}");
+        }
+
+        private static void InitializeServerData()
+        {
+            Clients = new Dictionary<int, Client>();
+
+            for (int i = 1; i <= MaxPlayers ; i++)
+            {
+                Clients.Add(i, new Client(i));
+            } 
+        }
+
+        public static void DisconnectClient(int id)
+        {
+            Clients[id]?.Disconnect();
+        }
+    }
+}
---
+++ MultiplayerGameServer/Assets/Scripts/Server/ServerSend.cs
@@ -0,0 +1,40 @@
+﻿using GameNetworkingShared.Objects;
+using GameNetworkingShared.Protocols;
+using System;
+
+namespace MultiplayerGameServer.Server
+{
+    public class ServerSend
+    {
+        internal static void Welcome(int clientId, string msg)
+        {
+            WelcomeMessage welcomeMessage = new WelcomeMessage()
+            {
+                Message = msg,
+                ClientId = clientId,
+            };
+
+            Server.Clients[clientId].Tcp.SendMessage(welcomeMessage);
+        }
+
+        internal static void UdpTest(int clientId)
+        {
+            UdpTest test = new UdpTest()
+            {
+                Message = "UDP_OK",
+            };
+
+            Server.UdpHandler.SendMessage(clientId, test);
+        }
+
+        internal static void SpawnPlayer(int id, Player player)
+        {
+            Server.Clients[id].Tcp.SendMessage(player);
+        }
+
+        internal static void UpdatePlayerPositionRotation(Player player)
+        {
+            Server.UdpHandler.SendMessageToAll(player.Position);
+        }
+    }
+}
