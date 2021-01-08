63cab3034145b3fa7815b692bf560943c6c94ab9
Nissan Cohen <nissanc6@gmail.com>
2021-01-09 00:14:31 +0200

    adding asteroids prefabs

--- MultiplayerGameServer/Assets/Scripts/Generic/ServerConsts.cs
+++ MultiplayerGameServer/Assets/Scripts/Generic/ServerConsts.cs
@@ -4,5 +4,8 @@
     {
         public const int TicksPerSecond = 30;
         public const double MsPerTick = 1000d / TicksPerSecond;
+
+        public const float PositiveBorder = 500f;
+        public const float NegativeBorder = -500f;
     }
 }
--- MultiplayerGameServer/Assets/Scripts/Server/Player.cs
+++ MultiplayerGameServer/Assets/Scripts/Server/Player.cs
@@ -2,6 +2,7 @@ using GameNetworkingShared.Logging;
 using GameNetworkingShared.Objects;
 using MultiplayerGameServer.Generic;
 using MultiplayerGameServer.Server;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -24,6 +25,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         acceleration = acceleration * Time.fixedDeltaTime * Time.fixedDeltaTime;
+        controller.detectCollisions = false;
     }
 
     void FixedUpdate()
@@ -43,8 +45,28 @@ public class Player : MonoBehaviour
         Vector3 moveDirection = new Vector3(xVelocity, yVelocity, 0f);
         controller.Move(moveDirection);
 
+        HandleReachingMapBorders();
+
         transform.rotation = Quaternion.Euler(new Vector3(Angle, 90f, -90f));
         ServerSend.UpdatePlayerPositionRotation(new PlayerPosition(Id, transform.position, Angle));
+    }
+
+    private void HandleReachingMapBorders()
+    {
+        float x = Mathf.Clamp(transform.position.x, ServerConsts.NegativeBorder, ServerConsts.PositiveBorder);
+        float y = Mathf.Clamp(transform.position.y, ServerConsts.NegativeBorder, ServerConsts.PositiveBorder);
+
+        if (x == ServerConsts.NegativeBorder || x == ServerConsts.PositiveBorder)
+        {
+            xVelocity = 0f;
+        }
+
+        if (y == ServerConsts.NegativeBorder || y == ServerConsts.PositiveBorder)
+        {
+            yVelocity = 0f;
+        }
+
+        transform.position = new Vector3(x, y, 0f);
     }
 
     private float CalculateVelocity(float currentVelocity, float direction)
